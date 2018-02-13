acfe40b780aab6686da4ae6e5d96d7c890506de9
Ulric Ye <yye90012@gmail.com>
2018-02-12 23:59:04 -0800

    Working on quagga api

--- public/js/barcode.js
+++ public/js/barcode.js
@@ -1,25 +1,58 @@
 'use strict';
 
-$(document).scannerDetection({
-    //https://github.com/kabachello/jQuery-Scanner-Detection
+// https://www.youtube.com/watch?v=qzbHxlQAa5o 
 
-    timeBeforeScanTest: 200, // wait for the next character for upto 200ms
-    avgTimeByChar: 40, // it's not a barcode if a character takes longer than 100ms
-    preventDefault: true,
-  
-    endChar: [13],
-        onComplete: function(barcode, qty){
-    validScan = true;
-     
-     
-        $('#scannerInput').val (barcode);
-      
-    } // main callback function ,
-    ,
-    onError: function(string, qty) {
-  
-    $('#userInput').val ($('#userInput').val()  + string);
-  
-      
+function order_by_occurence(arr) {
+    var counts = {};
+    arr.forEach(function(value) {
+        if(!counts[value]) {
+            counts[value] = 0;
+        }
+        counts[value] ++;
+    });
+
+    return Objects.keys(counts).sort(function(curKey, nextKey) {
+        return counts[curKey] < counts[nextKey];
+    });
+}
+
+function load_quagga() {
+    if($('#barcode-scanner').length>0 && navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia == 'function') {
+        var last_result = [];
+        if(Quagga.initialized == undefined) {
+            Quagga.onDetected(function(result) {
+                var last_code = result.codeResult.code;
+                last_result.push(last_code);
+                if(last_result.length > 20) {
+                    code = order_by_occurence(last_result)[0];
+                    last_result = [];
+                    Quagga.stop();
+                    $.ajax ({
+                        type: "POST", 
+                        url: '/products/get_barcode',
+                        data: { upc: last_code }
+                    });
+                }
+            });
+        }
+        
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                numOfWorkers: navigator.hardwareConcurrency,
+                target: document.querySelector('#barcode-scanner')
+            },
+            decoder: {
+                readers: ['ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'upc_reader', 'upc_e_reader']
+            }
+        }, function(err) {
+            if(err) { console.log(err); return }
+            Quagga.initialized = true;
+            Quagga.start();
+        });
+
     }
-});
+};
+
+$(document).on('turbolinks:load', load_quagga);
