2f4459b70083b232aba5c9bfb9d9662b0c1d76b0
inviswings <inviswings@gmail.com>
2018-02-15 21:21:13 -0800

    adding new search

--- routes/search.js
+++ routes/search.js
@@ -3,8 +3,24 @@
  * GET search page.
  */
 
+var data = require("../data.json");
+var suggest = [];
+
 
 exports.view = function(req, res){
     console.log("search");
-    res.render('search');
-  };
+    suggestion();
+    res.render('search', suggest);
+};
+
+
+
+function suggestion(){
+	for(var i in data.list){
+		if(data.list[i] === "5"){
+			suggest.push(data.list[i]);
+		}
+	}
+	return suggest;
+};
+
