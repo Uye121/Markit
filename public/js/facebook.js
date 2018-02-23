eb2622076eb76c097fa1e81a1969fb12efb60f89
Ulric Ye <yye90012@gmail.com>
2018-02-22 18:08:36 -0800

    Fixed FB login error

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -14,7 +14,9 @@ function checkLoginState() {
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
           console.log('Successfully logged in with Facebook');
-           FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+          //$.get('/index', changeUser);
+          window.location.href='/index';
+           //FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
     }
   }
   
