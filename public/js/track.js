0466dc228871e290315dd127a5aab3857efe9592
Ulric Ye <yye90012@gmail.com>
2018-03-06 14:19:22 -0800

    GA events sending fixed

--- public/js/track.js
+++ public/js/track.js
@@ -1,32 +1,36 @@
 'use strict';
 
 function track() {
-	$(".loading_image").click(function(e) {
-		console.log("stuff");
-		ga("send", "event", 'logo', 'click');
-    });
+	// $(".loading_image").click(function(e) {
+	// 	console.log("stuff");
+	// 	ga("send", "event", 'logo', 'click');
+    // });
     
-    $(".login").click(function(e) {
-        ga("send", "event", 'login', 'click');
-    });
+    // $(".login").click(function(e) {
+    //     ga("send", "event", 'login', 'click');
+    // });
 
-    $(".button").click(function(e) {
-        ga("send", "event", 'enterLogin', 'click');
-    });
+    // $(".button").click(function(e) {
+    //     ga("send", "event", 'enterLogin', 'click');
+    // });
 
-    $(".facebookLogin").click(function(e) {
-        ga("send", "event", 'fblogin', 'click');
-    });
+    // $(".facebookLogin").click(function(e) {
+    //     ga("send", "event", 'fblogin', 'click');
+    // });
 
-    $('.form-control').click(function() {
+    console.log("start tracking...");
+    $(".form-control").click(function() {
+        console.log("tap form");
         ga("send", "event", 'tap form', 'click');
     });
 
-    $('.btn #submitBtn').click(function() {
+    $(".btn #submitBtn").click(function() {
+        console.log("submit barcode");
         ga("send", "event", 'submit barcode', 'click');
     });
 
-    $('.scan_btn').click(function() {
+    $(".scan_btn").click(function() {
+        console.log("start scan");
         ga("send", "event", 'start scan', 'click');
     });
 }
